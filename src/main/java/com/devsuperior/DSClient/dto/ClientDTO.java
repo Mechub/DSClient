f1631c54e6bbe79c6c76daa4d489f830a3399070
Mercio franca <mercioalbuquerque@hotmail.com>
2023-11-15 20:25:05 -0300

    fix Beans

--- src/main/java/com/devsuperior/DSClient/dto/ClientDTO.java
+++ src/main/java/com/devsuperior/DSClient/dto/ClientDTO.java
@@ -6,25 +6,27 @@ import com.devsuperior.DSClient.entities.Client;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public class ClientDTO {
-
+    
     private Long id;
-
+    
     @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     @NotBlank(message = "Campo requerido")
     private String name;
 
+    @Size(min = 11, max = 11, message = "O CPF deve ter exatamente 11 caracteres")
     private String cpf;
 
+    @Positive(message = "A renda deve ser positiva")
     private Double income;
 
     @PastOrPresent(message = "A data de nascimento precisa ser válida")
     private LocalDate birthDate;
-
+    
     private Integer children;
-
     public ClientDTO(){
     }
     public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
@@ -36,14 +38,12 @@ public class ClientDTO {
         this.children = children;
     }
     public ClientDTO(Client entity){
-        
         id = entity.getId();
         name = entity.getName();
         cpf = entity.getCpf();
         income = entity.getIncome();
         birthDate = entity.getBirthDate();
         children = entity.getChildren();
-
     }
     public Long getId() {
         return id;
@@ -60,9 +60,7 @@ public class ClientDTO {
     public LocalDate getBirthDate() {
         return birthDate;
     }
-
     public Integer getChildren() {
         return children;
     } 
-
 }
