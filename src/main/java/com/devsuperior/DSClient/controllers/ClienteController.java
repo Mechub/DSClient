3d2e9de8a295c85ea9c94e5e5b2d20d6916d60e9
Mercio franca <mercioalbuquerque@hotmail.com>
2023-11-15 19:55:36 -0300

    POST new Client

--- src/main/java/com/devsuperior/DSClient/controllers/ClienteController.java
+++ src/main/java/com/devsuperior/DSClient/controllers/ClienteController.java
@@ -5,26 +5,33 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.DSClient.dto.ClientDTO;
+import com.devsuperior.DSClient.service.ClientService;
 
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
-
     @Autowired
     private ClientService service;
-
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id){
         return service.findById(id);
     }
-
+    
     @GetMapping
     public Page<ClientDTO> findAll(Pageable pageable){
         return service.findAll(pageable);
     }
 
+      @PostMapping
+        public ClientDTO insert(@RequestBody ClientDTO dto){
+            return service.insert(dto);
+        }
 
 }
